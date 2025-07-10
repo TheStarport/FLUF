c601eb68ccbfdd8ad08bf4a3bd141f8bd7fc6db1
Laz <lazdisco@gmail.com>
2025-07-10 22:25:08 +0100

    fix: adjust rendering of imgui notifications, allow notifications to be right or left aligned

--- FLUF.UI/Include/ImGui/ImGuiNotify.hpp
+++ FLUF.UI/Include/ImGui/ImGuiNotify.hpp
@@ -29,8 +29,6 @@
  */
 
 #define NOTIFY_MAX_MSG_LENGTH     4096  // Max message content length
-#define NOTIFY_PADDING_X          20.f  // Bottom-left X padding
-#define NOTIFY_PADDING_Y          20.f  // Bottom-left Y padding
 #define NOTIFY_PADDING_MESSAGE_Y  10.f  // Padding Y between each message
 #define NOTIFY_FADE_IN_OUT_TIME   150   // Fade in and out duration
 #define NOTIFY_DEFAULT_DISMISS    3000  // Auto dismiss after X ms (default, applied only of no data provided in constructors)
@@ -78,18 +76,6 @@ enum class ImGuiToastPhase : uint8_t
     Wait,
     FadeOut,
     Expired,
-    COUNT
-};
-
-enum class ImGuiToastPos : uint8_t
-{
-    TopLeft,
-    TopCenter,
-    TopRight,
-    BottomLeft,
-    BottomCenter,
-    BottomRight,
-    Center,
     COUNT
 };
 
@@ -416,7 +402,7 @@ namespace ImGui
      * Each notification is rendered as a toast window with a title, content and an optional icon.
      * If a notification is expired, it is removed from the vector.
      */
-    inline void RenderNotifications()
+    inline void RenderNotifications(const bool rightAligned = false)
     {
         const ImVec2 mainWindowSize = GetMainViewport()->Size;
 
@@ -454,38 +440,28 @@ namespace ImGui
 
             // Generate new unique name for this toast
             char windowName[50];
-#ifdef _WIN32
             sprintf_s(windowName, "##TOAST%d", (int)i);
-#elif defined(__linux__) || defined(__EMSCRIPTEN__)
-            std::sprintf(windowName, "##TOAST%d", (int)i);
-#elif defined(__APPLE__)
-            std::snprintf(windowName, 50, "##TOAST%d", (int)i);
-#else
-            throw "Unsupported platform";
-#endif
 
             // PushStyleColor(ImGuiCol_Text, textColor);
             SetNextWindowBgAlpha(opacity);
 
-#if NOTIFY_RENDER_OUTSIDE_MAIN_WINDOW
             short mainMonitorId = static_cast<ImGuiViewportP*>(GetMainViewport())->PlatformMonitor;
 
             ImGuiPlatformIO& platformIO = GetPlatformIO();
             ImGuiPlatformMonitor& monitor = platformIO.Monitors[mainMonitorId];
 
-            // Set notification window position to bottom right corner of the monitor
-            SetNextWindowPos(
-                ImVec2(monitor.WorkPos.x + monitor.WorkSize.x - NOTIFY_PADDING_X, monitor.WorkPos.y + monitor.WorkSize.y - NOTIFY_PADDING_Y - height),
-                ImGuiCond_Always,
-                ImVec2(1.0f, 1.0f));
-#else
-            // Set notification window position to bottom right corner of the main window, considering the main window size and location in relation to the
-            // display
-            ImVec2 mainWindowPos = GetMainViewport()->Pos;
-            SetNextWindowPos(ImVec2(mainWindowPos.x + mainWindowSize.x - NOTIFY_PADDING_X, mainWindowPos.y + mainWindowSize.y - NOTIFY_PADDING_Y - height),
-                             ImGuiCond_Always,
-                             ImVec2(1.0f, 1.0f));
-#endif
+            if (rightAligned)
+            {
+                // Set notification window position to bottom right corner of the monitor
+                SetNextWindowPos(ImVec2((monitor.WorkPos.x + monitor.WorkSize.x) * 0.95f, (monitor.WorkPos.y + monitor.WorkSize.y) * 0.1f + height),
+                                 ImGuiCond_Always,
+                                 ImVec2(1.0f, 1.0f));
+            }
+            else
+            {
+                SetNextWindowPos(ImVec2((monitor.WorkPos.x + monitor.WorkSize.x) * 0.02f, (monitor.WorkPos.y + monitor.WorkSize.y) * 0.05f + height),
+                                 ImGuiCond_Always);
+            }
 
             // Set notification window flags
             if (!NOTIFY_USE_DISMISS_BUTTON && currentToast->getOnButtonPress() == nullptr)
@@ -499,98 +475,96 @@ namespace ImGui
             BringWindowToDisplayFront(GetCurrentWindow());
 
             // Here we render the toast content
+            PushTextWrapPos(mainWindowSize.x / 3.f); // We want to support multi-line text, this will wrap the text after 1/3 of the screen width
+
+            bool wasTitleRendered = false;
+
+            // If an icon is set
+            if (!NOTIFY_NULL_OR_EMPTY(icon))
             {
-                PushTextWrapPos(mainWindowSize.x / 3.f); // We want to support multi-line text, this will wrap the text after 1/3 of the screen width
+                // Text(icon); // Render icon text
+                TextColored(textColor, "%s", icon);
+                wasTitleRendered = true;
+            }
 
-                bool wasTitleRendered = false;
-
-                // If an icon is set
+            // If a title is set
+            if (!NOTIFY_NULL_OR_EMPTY(title))
+            {
+                // If a title and an icon is set, we want to render on same line
                 if (!NOTIFY_NULL_OR_EMPTY(icon))
                 {
-                    // Text(icon); // Render icon text
-                    TextColored(textColor, "%s", icon);
-                    wasTitleRendered = true;
+                    SameLine();
                 }
 
-                // If a title is set
-                if (!NOTIFY_NULL_OR_EMPTY(title))
-                {
-                    // If a title and an icon is set, we want to render on same line
-                    if (!NOTIFY_NULL_OR_EMPTY(icon))
-                    {
-                        SameLine();
-                    }
-
-                    Text("%s", title); // Render title text
-                    wasTitleRendered = true;
-                }
-                else if (!NOTIFY_NULL_OR_EMPTY(defaultTitle))
-                {
-                    if (!NOTIFY_NULL_OR_EMPTY(icon))
-                    {
-                        SameLine();
-                    }
-
-                    Text("%s", defaultTitle); // Render default title text (ImGuiToastType_Success -> "Success", etc...)
-                    wasTitleRendered = true;
-                }
-
-                // If a dismiss button is enabled
-                if (NOTIFY_USE_DISMISS_BUTTON)
-                {
-                    // If a title or content is set, we want to render the button on the same line
-                    if (wasTitleRendered || !NOTIFY_NULL_OR_EMPTY(content))
-                    {
-                        SameLine();
-                    }
-
-                    // Render the dismiss button on the top right corner
-                    // NEEDS TO BE REWORKED
-                    float scale = 1.0f;
-
-                    if (CalcTextSize(content).x > GetContentRegionAvail().x)
-                    {
-                        scale = 0.95f;
-                    }
-
-                    SetCursorPosX(GetCursorPosX() + (GetWindowSize().x - GetCursorPosX()) * scale);
-
-                    // If the button is pressed, we want to remove the notification
-                    if (Button(ICON_FA_XMARK))
-                    {
-                        RemoveNotification(i);
-                    }
-                }
-
-                // In case ANYTHING was rendered in the top, we want to add a small padding so the text (or icon) looks centered vertically
-                if (wasTitleRendered && !NOTIFY_NULL_OR_EMPTY(content))
-                {
-                    SetCursorPosY(GetCursorPosY() + 5.f); // Must be a better way to do this!!!!
-                }
-
-                // If a content is set
-                if (!NOTIFY_NULL_OR_EMPTY(content))
-                {
-                    if (wasTitleRendered && addSeperator)
-                    {
-                        Separator();
-                    }
-
-                    Text("%s", content); // Render content text
-                }
-
-                // If a button is set
-                if (currentToast->getOnButtonPress() != nullptr)
-                {
-                    // If the button is pressed, we want to execute the lambda function
-                    if (Button(currentToast->getButtonLabel()))
-                    {
-                        currentToast->getOnButtonPress()();
-                    }
-                }
-
-                PopTextWrapPos();
+                Text("%s", title); // Render title text
+                wasTitleRendered = true;
             }
+            else if (!NOTIFY_NULL_OR_EMPTY(defaultTitle))
+            {
+                if (!NOTIFY_NULL_OR_EMPTY(icon))
+                {
+                    SameLine();
+                }
+
+                Text("%s", defaultTitle); // Render default title text (ImGuiToastType_Success -> "Success", etc...)
+                wasTitleRendered = true;
+            }
+
+            // If a dismiss button is enabled
+            if (NOTIFY_USE_DISMISS_BUTTON)
+            {
+                // If a title or content is set, we want to render the button on the same line
+                if (wasTitleRendered || !NOTIFY_NULL_OR_EMPTY(content))
+                {
+                    SameLine();
+                }
+
+                // Render the dismiss button on the top right corner
+                // NEEDS TO BE REWORKED
+                float scale = 0.85f;
+
+                if (CalcTextSize(content).x > GetContentRegionAvail().x)
+                {
+                    scale = 0.85f;
+                }
+
+                SetCursorPosX(GetCursorPosX() + (GetWindowSize().x - GetCursorPosX()) * scale);
+
+                // If the button is pressed, we want to remove the notification
+                if (Button(ICON_FA_XMARK))
+                {
+                    RemoveNotification(i);
+                }
+            }
+
+            // In case ANYTHING was rendered in the top, we want to add a small padding so the text (or icon) looks centered vertically
+            if (wasTitleRendered && !NOTIFY_NULL_OR_EMPTY(content))
+            {
+                SetCursorPosY(GetCursorPosY() + 5.f); // Must be a better way to do this!!!!
+            }
+
+            // If a content is set
+            if (!NOTIFY_NULL_OR_EMPTY(content))
+            {
+                if (wasTitleRendered && addSeperator)
+                {
+                    Separator();
+                }
+
+                Text("%s", content); // Render content text
+            }
+
+            // If a button is set
+            if (currentToast->getOnButtonPress() != nullptr)
+            {
+                // If the button is pressed, we want to execute the lambda function
+                if (Button(currentToast->getButtonLabel()))
+                {
+                    currentToast->getOnButtonPress()();
+                }
+            }
+
+            PopTextWrapPos();
 
             // Save height for next toasts
             height += GetWindowHeight() + NOTIFY_PADDING_MESSAGE_Y;
