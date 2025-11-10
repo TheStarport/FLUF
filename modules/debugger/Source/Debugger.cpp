cc8141470fc6dacfa2a2ba3d7e9c367c7ab9d920
Aingar <47479741+Aingar@users.noreply.github.com>
2025-11-10 01:12:23 +0100

    fix: debugger crashes

--- modules/debugger/Source/Debugger.cpp
+++ modules/debugger/Source/Debugger.cpp
@@ -42,10 +42,8 @@ void Debugger::RenderDebugSpaceWindow(Ship* player)
 
     EqObj* target = nullptr;
     player->get_target(reinterpret_cast<IObjRW*&>(target));
-    if (target && !target->is_dying())
+    if (target && !target->is_dying() && target->cobj->archetype->archId != 0xb740ccc5) // waypoint check
     {
-        ImGui::BeginTable("##dbg-target-table", 2);
-
         auto eq = target->ceqobj();
 
         const auto shield = reinterpret_cast<CEShield*>(eq->equipManager.FindFirst(static_cast<uint>(EquipmentClass::Shield)));
@@ -65,8 +63,17 @@ void Debugger::RenderDebugSpaceWindow(Ship* player)
         float power = 0.f;
         target->get_power(power);
 
+        std::string_view behaviourName = "";
         auto ai = eq->get_behavior_interface();
-        auto behaviourName = magic_enum::enum_name(ai->get_current_directive()->op);
+
+        if (ai)
+        {
+            behaviourName = magic_enum::enum_name(ai->get_current_directive()->op);
+        }
+        else
+        {
+            behaviourName = "No AI";
+        }
 
         auto vibe = eq->repVibe;
         std::string affiliationStr = "Unknown";
@@ -76,25 +83,37 @@ void Debugger::RenderDebugSpaceWindow(Ship* player)
         FmtStr name2(0, nullptr);
         const ushort* str = nullptr;
         Reputation::Vibe::GetName(vibe, name1, name2, str);
+        Reputation::Vibe::GetAffiliation(vibe, affiliation, true);
 
-        if (_wcsicmp(reinterpret_cast<const wchar_t*>(str), L"(null)") == 0)
+        if (affiliation)
         {
-            Reputation::Vibe::GetAffiliation(vibe, affiliation, true);
-
-            if (affiliation)
-            {
-                affiliationStr = StringUtils::wstos(Fluf::GetInfocardName(Reputation::get_short_name(affiliation)));
-            }
+            affiliationStr = StringUtils::wstos(Fluf::GetInfocardName(Reputation::get_short_name(affiliation)));
         }
 
         std::string archStr = StringUtils::wstos(Fluf::GetInfocardName(eq->archetype->idsName));
 
+        ImGui::BeginTable("##dbg-target-table", 2);
+
         ImKvp("ID", "%u", target->get_id());
-        ImKvp("Name", "%s", StringUtils::wstos(reinterpret_cast<const wchar_t*>(str)));
-        ImKvp("Affiliation", "%s", affiliationStr);
+        if (eq->objectClass == CObject::CSOLAR_OBJECT)
+        {
+            ImKvp("Name", "%s", StringUtils::wstos(Fluf::GetInfocardName(name1.strid)).c_str());
+        }
+        else
+        {
+            ImKvp("Name", "%s", StringUtils::wstos(reinterpret_cast<const wchar_t*>(str)).c_str());
+        }
+        ImKvp("Affiliation", "%s", affiliationStr.c_str());
         ImKvp("AI State", "%s", behaviourName.empty() ? "Unknown" : behaviourName.data());
         ImKvp("Velocity", "%.0f", target->get_velocity().Magnitude());
-        ImKvp("Health", "%.0f (%.0f%%)", eq->hitPoints, eq->get_relative_health() * 100.f);
+        if (eq->objectClass == CObject::CSOLAR_OBJECT && !reinterpret_cast<CSolar*>(eq)->isDestructible)
+        {
+            ImKvp("Health", "Indestructible");
+        }
+        else
+        {
+            ImKvp("Health", "%.0f (%.0f%%)", eq->hitPoints, eq->get_relative_health() * 100.f);
+        }
         ImKvp("Shield", "%.0f (%s)", shieldCurrent, shieldRecharging ? "Offline" : "Online");
         ImKvp("Power", "%.0f", power);
 
@@ -153,6 +172,7 @@ void Debugger::RenderDebugSpaceWindow(Ship* player)
         ImGui::Text("No target selected");
     }
 
+  earlyExit:
     ImGui::PopStyleVar();
     ImGui::PopStyleColor();
     ImGui::End();
